7036ec624faae64aa4c43eef636363c10e63aea8
dudamatos <dudasantosmatos15@gmail.com>
2024-09-30 21:56:02 -0300

    changed

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Responses\ApiResponse;
+use Illuminate\Http\Request;
+use Response;
+
+class ClienteController extends Controller
+{
+    public function salvar()
+    {
+
+    }
+       public function listar()
+    {
+      return ApiResponse::success(data: ["nome" => "Eduarda"]);
+    }
+     public function editar(int $id)
+    {
+
+    }
+
+    public function excluir(int $id)
+    {
+
+    }
+}
